cba2433380c259fbb9d4789374fd3f2ca5c7ba8b
Przemek <100935707+PrzemekSwiecinski@users.noreply.github.com>
2026-05-31 01:07:05 +0200

    Update zapisz_kolory.php

--- api/zapisz_kolory.php
+++ api/zapisz_kolory.php
@@ -4,7 +4,6 @@ header('Access-Control-Allow-Origin: *');
 header('Access-Control-Allow-Methods: POST');
 header('Access-Control-Allow-Headers: Content-Type');
 
-// Odczytaj dane z żądania
 $data = json_decode(file_get_contents('php://input'), true);
 
 $primaryColor = $data['primaryColor'] ?? null;
@@ -17,14 +16,11 @@ if ($primaryColor !== null && $secondaryColor !== null && $textColor !== null) {
 
     $cssContent = file_get_contents($cssFilePath);
 
-  // Zmiana kolorów w zawartości pliku
   $cssContent = preg_replace('/--primary-color:\s*#[0-9a-fA-F]{6};/', '--primary-color: ' . $primaryColor . ';', $cssContent);
     $cssContent = preg_replace('/--secondary-color:\s*#[0-9a-fA-F]{6};/', '--secondary-color: ' . $secondaryColor . ';', $cssContent);
     $cssContent = preg_replace('/--text-color:\s*#[0-9a-fA-F]{6};/', '--text-color: ' . $textColor . ';', $cssContent);
 
 
-
-    // Zapisz zmienioną zawartość z powrotem do pliku
     if (file_put_contents($cssFilePath, $cssContent) !== false) {
          echo json_encode(['message' => 'Colors updated successfully']);
       } else {
